0b588d22cbd2c59ca927bde1fc9808c57e154f5d
Harshad Deo <subterranean.hominid@gmail.com>
2020-12-07 17:19:43 +0530

    add lower bound vdistribution

--- torchvi/vdistributions/__init__.py
+++ torchvi/vdistributions/__init__.py
@@ -1,5 +1,7 @@
 from torchvi.vdistributions import beta
 from torchvi.vdistributions import constant
+from torchvi.vdistributions import half_normal
 
 Beta = beta.Beta
 Constant = constant.Constant
+HalfNormal = half_normal.HalfNormal
---
+++ torchvi/vdistributions/half_normal.py
@@ -0,0 +1,31 @@
+import torch
+from torch import distributions
+
+from torchvi.vmodule import VModule
+from torchvi.vtensor.lowerbound import LowerBound
+from torchvi.vdistributions.constant import wrap_if_constant
+
+
+class HalfNormal(VModule):
+    def __init__(self, size, scale):
+        super().__init__()
+        self.size = size
+        self.scale = wrap_if_constant(scale)
+        self.node = LowerBound(size, 0)
+
+    def forward(self, x):
+        zeta, constraint_contrib = self.node.forward(x)
+
+        scale, scale_constraint = self.scale.forward(x)
+        constraint_contrib += scale_constraint
+
+        prior = distributions.HalfNormal(scale)
+        constraint_contrib += prior.log_prob(zeta).sum()
+
+        return zeta, constraint_contrib
+
+    def sample(self, x, size):
+        return self.node.sample(x, size)
+
+    def extra_repr(self):
+        return f'size={self.size}, loc={self.loc}, scale={self.scale}'
